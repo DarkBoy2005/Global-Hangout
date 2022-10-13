d3ca32d8a3b841173000df02326d472b7c33c51c
DarkBoy2005 <68329594+DarkBoy2005@users.noreply.github.com>
2022-10-13 18:22:29 +0200

    Update messageReactionRemove.js

--- events/guild/messageReactionRemove.js
+++ events/guild/messageReactionRemove.js
@@ -9,24 +9,4 @@
  const db = require('quick.db');
  //here the event starts
  module.exports = async (client, reaction, user) => {
-   try {
-      if (user.bot) return;
-      const loadingemoji = client.emojis.cache.get("993229855443398656")
-      const message = reaction.message
-      const emoji = reaction.emoji
-      const server = client.guilds.cache.get(reaction.message.guild.id)
-      const roleid = db.get(`${server.id}-${message.id}-${emoji.id}.roleId`)
-      if(roleid) {
-         let role = server.roles.cache.find(role => role.id === roleid)
-         let member = server.members.cache.get(user.id)
-         member.roles.remove(roleid)
-         reaction.message.channel.send({content: `${loadingemoji} \`Removing ${role.name} role...\` | The role will be removed from you shortly <@${user.id}>...`}).then(msg => {
-            setTimeout(() => {
-               msg.delete()
-            }, 3000)
-         })
-      }
-   } catch (e) {
-      console.log(e)
-   }
 }
